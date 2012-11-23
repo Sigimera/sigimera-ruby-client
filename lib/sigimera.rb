58b4737af9efd310f4a32967cb47549c2a4b70bf
Alex Oberhauser <alex.oberhauser@sigimera.org>
2012-11-23 22:44:03 +0100

    Add missing documentation for two constants.

--- lib/sigimera.rb
+++ lib/sigimera.rb
@@ -7,7 +7,10 @@
 # Copyright::   Copyright (c) 2012 Sigimera
 # License::     MIT
 module Sigimera
+	# The Sigimera REST API Host
 	API_HOST 	= "https://api.sigimera.org"
+
+	# The used SSL certificate for the HTTPS encrpytion
 	CACERT_FILE	= File.expand_path("../../certs/cacert.crt", __FILE__)
 end
 
